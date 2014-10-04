fd585a2749e4481efd7168d6f80ed1f24b8db527
akilman <anthony.kilman@gmail.com>
2014-10-04 14:18:50 -0700

    First cut of the Go bank account app
    Illustrates basic usage of 'go func's, channels, and select

--- go/src/bankaccount/app.go
+++ go/src/bankaccount/app.go
@@ -1,7 +1,60 @@
 package main
 
-import "fmt"
+import (
+		"time" 
+		"fmt"
+		"math/rand"
+)
 
 func main() {
-    fmt.Printf("Hello world\n")
+    
+	rand.Seed(42)
+
+	var nDeposits int = rand.Intn(100)	
+	var nWithdrawls int = rand.Intn(100)
+
+	// Two channels for deposits and withdrawls, respective
+	deposits := make(chan int)
+	withdrawls := make(chan int)
+
+	// go function for processing deposits
+	go func() {
+		for i := 0; i < nDeposits; i++ {
+			time.Sleep(time.Duration(rand.Intn(1000)))
+			deposits <- 1
+		}
+	}()
+
+	// go function for processing withdrawls
+	go func() { 
+		for i := 0; i < nWithdrawls; i++ {
+			time.Sleep(time.Duration(rand.Intn(1000)))
+			withdrawls <- 1
+		}
+	}()
+
+	// our "bank account"
+	var balance int = 0
+
+	var nTransactions int = nDeposits + nWithdrawls
+
+	// loop through, use select construct to process transactions
+	for i := 0; i < nTransactions; i++ {
+		select {
+		case deposit := <- deposits:
+			balance += deposit
+			fmt.Printf("Deposited $%v\n", deposit)
+
+		case withdrawl := <- withdrawls:
+
+			if (withdrawl > balance) {
+				fmt.Printf("Unable to withdraw $%v. Insufficient funds!\n", withdrawl)
+			} else {
+				balance -= withdrawl
+				fmt.Printf("Withdrew $%v\n", withdrawl)
+			}
+		}
+	}
+
+	fmt.Printf("Remaining balance: $%v\n", balance)
 }
