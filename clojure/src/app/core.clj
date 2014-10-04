d1c880a484d01d7076f4752b4f8808a5908d0c24
akilman <anthony.kilman@gmail.com>
2014-10-04 09:26:59 -0700

    Really basic beginning agent bank account example

--- clojure/src/app/core.clj
+++ clojure/src/app/core.clj
@@ -2,7 +2,25 @@
   (:refer-clojure :exclude [map reduce into partition partition-by take merge])
   (:require [clojure.core.async :refer :all :as async]))
 
-(let [c (chan 10)]
-  (>!! c "hello")
-  (println (<!! c))
-  (close! c))
+; function to add some value 'c' to 'n'
+(defn increment [c n] (+ c n))
+
+(defn example []
+  (let [
+         ; start with a bank account of balance zero
+         bank-account (agent 0)]
+
+    (send bank-account increment 5)
+
+    (Thread/sleep 1000)
+
+    ; print output
+    (println @bank-account)))
+
+; onward!
+(example)
+(example)
+(example)
+(example)
+(example)
+(example)
