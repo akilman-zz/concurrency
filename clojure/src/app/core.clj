21bab0ab18c88d83a2f8988ab8726aa7866a15a6
akilman <anthony.kilman@gmail.com>
2014-10-04 10:41:53 -0700

    Better update code documentation

--- clojure/src/app/core.clj
+++ clojure/src/app/core.clj
@@ -16,6 +16,11 @@
 (defn sleep-for [n]
   (Thread/sleep n))
 
+;;; Example of using Agents and the Clojure STM to simulate a bank account use case
+;;;
+;;; Two threads of execution are spawned: (1) deposits, (2) via withdrawls via futures.
+;;; Each logical thread of execution sleeps for a short random internal and updates the
+;;; bank account balance via the 'send' function
 (defn example []
 
   ;; start with a bank account of balance zero
@@ -32,6 +37,7 @@
         (sleep-for (rand 500))
         (send bank-account withdraw 5)))
 
+    ;; futures are not guaranteed to execute immediately, thus the explicit wait
     (sleep-for 5000)
 
     ;; print output
